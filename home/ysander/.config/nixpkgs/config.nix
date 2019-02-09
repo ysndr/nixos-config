78a16b0f4dce5e43035343b5731b0efce979f096
Yannik Sander <me@ysndr.de>
2019-02-09 01:10:57 +0100

    Bundle gnome extensions with firefox

--- home/ysander/.config/nixpkgs/config.nix
+++ home/ysander/.config/nixpkgs/config.nix
@@ -23,6 +23,7 @@
     gssSupport = true;
     /* jre = true; */
     /* enableMPlayer = true; */
+    enableGnomeExtensions = true;
     enableDjvu = true;
     enablePlasmaBrowserIntegration = true;
     enableBrowserpass = true;
