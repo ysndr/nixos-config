b6a7dbd6e374da6f7a1e2724f1d38eb43f78c960
Yannik Sander <me@ysndr.de>
2019-02-09 01:10:31 +0100

    Configure caches for haskell hie

--- home/ysander/.config/nixpkgs/config.nix
+++ home/ysander/.config/nixpkgs/config.nix
@@ -27,4 +27,16 @@
     enablePlasmaBrowserIntegration = true;
     enableBrowserpass = true;
   };
+
+  nix = {
+    binaryCaches = [
+      "https://cache.nixos.org/"
+      "https://hie-nix.cachix.org"
+    ];
+    binaryCachePublicKeys = [
+      "hie-nix.cachix.org-1:EjBSHzF6VmDnzqlldGXbi0RM3HdjfTU3yDRi9Pd0jTY="
+    ];
+    trustedUsers = [ "root" "ysander" ];
+  };
+
 }
