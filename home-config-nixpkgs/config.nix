2528001c9be702c70e2f9b6da4cc85c049faa2cf
ysander <ysander@techfak.uni-bielefeld.de>
2019-10-04 21:25:07 +0200

    Expand nix config file by linux specific overrides

--- home-config-nixpkgs/config.nix
+++ home-config-nixpkgs/config.nix
@@ -15,4 +15,28 @@
     own = nur.repos.ysndr;
     nur-local = nur.repos.local;
   };
-}
+} // (if builtins.currentSystem == "x86_64-linux" then {
+  packageOverrides = super: rec {
+    steam = super.steam.override {
+      withPrimus = true;
+      extraPkgs = p: with p; [
+        glxinfo        # for diagnostics
+        nettools       # for `hostname`, which some scripts expect
+        # bumblebee      # for optirun
+      ];
+    };
+  };
+  
+  firefox = {
+    ffmpegSupport = true;
+    gssSupport = true;
+    /* jre = true; */
+    /* enableMPlayer = true; */
+    enableGnomeExtensions = true;
+    enableDjvu = true;
+    enablePlasmaBrowserIntegration = true;
+    enableBrowserpass = true;
+  };
+  android_sdk.accept_license = true;
+
+} else {})
